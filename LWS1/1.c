80a2fec507e61e075bfcc6e79df82ef94bcdf46a
Pamod Malith <dev.pamodmalith@gmail.com>
2026-01-08 11:42:39 +0530

    update question one

--- LWS1/1.c
+++ LWS1/1.c
@@ -1,9 +1,13 @@
-#include <string.h>
 #include <stdio.h>
 
-int main(void)
+void main()
 {
-  char x = 'x';
-  printf("ascii value of x = %d\n", x);
-  return 0;
+  char s1[10], s2[15];
+
+  printf("Enter the string \"Word Processing\" in two parts: ");
+  scanf("%s %s", s1, s2);
+
+  printf("%s %s\n", s1, s2);
+  printf("%s\n%s\n", s1, s2);
+  printf("%.1s.%.1s.\n", s1, s2);
 }
